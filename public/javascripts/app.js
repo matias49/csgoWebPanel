83b68042eae396bde5ebe086758afa1dc9b81efc
Matias49 <matias49@live.fr>
2016-03-11 17:16:12 +0100

    More Style
    Background for teams
    Add bars for health and armor
    Add option to hide important information if players can see the screen

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,6 +1,20 @@
 var socket = io.connect('http://localhost:3001');
 
-socket.on('info', function(data){
+socket.on('info', function(data) {
   console.log(data.text);
-  $(".data").append(data.text+ "<br />");
+  $(".data").append(data.text + "<br />");
+});
+
+$(document).ready(function() {
+  console.log('ready');
+});
+
+$('#playersSeeScoreboard').click(function() {
+  if ($('#playersSeeScoreboard').is(':checked')) {
+    $('.playerBars').hide();
+    $('.playerMoney').hide();
+  } else {
+    $('.playerBars').show();
+    $('.playerMoney').show();
+  }
 });
