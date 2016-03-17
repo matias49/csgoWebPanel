399aebcb028a3bdbf98c50bcb58af0c9b997dc93
Matias49 <matias49@live.fr>
2016-03-17 11:28:24 +0100

    Inverted TeamT & TeamCT location on code
    Player is an id

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -9,36 +9,36 @@ socket.on('mapInfo', function(data) {
   $("#status").text(data.status);
 });
 
-socket.on('teamT', function(data) {
+socket.on('teamCT', function(data) {
   $("#team1.team").text(data.name);
   $("#team1.score").text(data.score);
 });
 
-socket.on('teamCT', function(data) {
-  $("#team1.team").text(data.name);
-  $("#team1.score").text(data.score);
+socket.on('teamT', function(data) {
+  $("#team2.team").text(data.name);
+  $("#team2.score").text(data.score);
 });
 
 socket.on('players', function(data) {
   for (var i = 0; i < data.players.length; i++) {
     player = data.players[i];
     console.log("Player " + player.name + "is on team" + player.team);
-    $(".player" + i + ".playerName").text(player.name);
-    $(".player" + i + ".playerMoney").text(player.money);
+    $("#player" + i + ".playerName").text(player.name);
+    $("#player" + i + ".playerMoney").text(player.money);
     // Health
-    $(".player" + i + ".playerHealth.progress-bar").attr("aria-valuenow", player.health);
-    $(".player" + i + ".playerHealth.progress-bar").css("width", player.health + "%");
-    $(".player" + i + ".playerHealth.progress-bar").text(player.health);
+    $("#player" + i + ".playerHealth.progress-bar").attr("aria-valuenow", player.health);
+    $("#player" + i + ".playerHealth.progress-bar").css("width", player.health + "%");
+    $("#player" + i + ".playerHealth.progress-bar").text(player.health);
     // Armor
-    $(".player" + i + ".playerArmor.progress-bar").attr("aria-valuenow", player.armor);
-    $(".player" + i + ".playerArmor.progress-bar").css("width", player.armor + "%");
-    $(".player" + i + ".playerArmor.progress-bar").text(player.armor);
+    $("#player" + i + ".playerArmor.progress-bar").attr("aria-valuenow", player.armor);
+    $("#player" + i + ".playerArmor.progress-bar").css("width", player.armor + "%");
+    $("#player" + i + ".playerArmor.progress-bar").text(player.armor);
 
     // More info
-    $(".player" + i + ".playerKills").text(player.kills);
-    $(".player" + i + ".playerName").text(player.deaths);
-    $(".player" + i + ".playerName").text(player.assists);
-    $(".player" + i + ".playerName").text(player.score);
+    $("#player" + i + ".playerKills").text(player.kills);
+    $("#player" + i + ".playerName").text(player.deaths);
+    $("#player" + i + ".playerName").text(player.assists);
+    $("#player" + i + ".playerName").text(player.score);
     // $(".player"+i+".playerName").text(player.roundKills);
   }
 });
