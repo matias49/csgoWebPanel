28a31e05889cbaad1fa1246a327239045756b615
Matias49 <matias49@live.fr>
2016-03-17 12:20:40 +0100

    Fixed JQuery CSS

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,7 +1,7 @@
 var socket = io.connect('http://localhost:3001');
 
 socket.on('info', function(data) {
-  $(".data").append(data.text + "<br />");
+  $("#logText").append(data.text + "<br />");
 });
 
 socket.on('mapInfo', function(data) {
@@ -10,36 +10,36 @@ socket.on('mapInfo', function(data) {
 });
 
 socket.on('teamCT', function(data) {
-  $("#team1.team").text(data.name);
-  $("#team1.score").text(data.score);
+  $("#team1 .team").text(data.name);
+  $("#team1 .score").text(data.score);
 });
 
 socket.on('teamT', function(data) {
-  $("#team2.team").text(data.name);
-  $("#team2.score").text(data.score);
+  $("#team2 .team").text(data.name);
+  $("#team2 .score").text(data.score);
 });
 
 socket.on('players', function(data) {
   for (var i = 0; i < data.players.length; i++) {
-    player = data.players[i];
-    console.log("Player " + player.name + "is on team" + player.team);
-    $("#player" + i + ".playerName").text(player.name);
-    $("#player" + i + ".playerMoney").text(player.money);
+    var player = data.players[i];
+    console.log("Player " + player.name + "is on team " + player.team);
+    $("#player" + i + " .playerInfo .playerName").text(player.name);
+    $("#player" + i + " .playerMoney").text(player.money);
     // Health
-    $("#player" + i + ".playerHealth.progress-bar").attr("aria-valuenow", player.health);
-    $("#player" + i + ".playerHealth.progress-bar").css("width", player.health + "%");
-    $("#player" + i + ".playerHealth.progress-bar").text(player.health);
+    $("#player" + i + " .playerHealth .progress-bar").attr("aria-valuenow", player.health);
+    $("#player" + i + " .playerHealth .progress-bar").css("width", player.health + "%");
+    $("#player" + i + " .playerHealth .progress-bar").text(player.health);
     // Armor
-    $("#player" + i + ".playerArmor.progress-bar").attr("aria-valuenow", player.armor);
-    $("#player" + i + ".playerArmor.progress-bar").css("width", player.armor + "%");
-    $("#player" + i + ".playerArmor.progress-bar").text(player.armor);
+    $("#player" + i + " .playerArmor .progress-bar").attr("aria-valuenow", player.armor);
+    $("#player" + i + " .playerArmor .progress-bar").css("width", player.armor + "%");
+    $("#player" + i + " .playerArmor .progress-bar").text(player.armor);
 
     // More info
-    $("#player" + i + ".playerKills").text(player.kills);
-    $("#player" + i + ".playerName").text(player.deaths);
-    $("#player" + i + ".playerName").text(player.assists);
-    $("#player" + i + ".playerName").text(player.score);
-    // $(".player"+i+".playerName").text(player.roundKills);
+    $("#player" + i + " .playerKills").text(player.kills);
+    $("#player" + i + " .playerDeaths").text(player.deaths);
+    $("#player" + i + " .playerAssists").text(player.assists);
+    $("#player" + i + " .playerScore").text(player.score);
+    // $(".player"+i+" .playerName").text(player.roundKills);
   }
 });
 
