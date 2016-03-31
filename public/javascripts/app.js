eb3607f47396271c59c5febf1d1e3ab719fdd2f1
Matias49 <matias49@live.fr>
2016-03-31 11:42:22 +0200

    Beautify

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -23,14 +23,14 @@ socket.on('teamT', function(data) {
 });
 
 // We send the notification to the user only if he isn't watching the tab
-socket.on('notification', function(data){
-  if(!document.hasFocus()){
+socket.on('notification', function(data) {
+  if (!document.hasFocus()) {
     notifyUser(data.text);
   }
 })
 
-socket.on('swapTeams', function(data){
-  if((data.round >= 16 && data.phase != 'over' && !teamSwaped) || (data.round <= 15 && teamSwaped)){
+socket.on('swapTeams', function(data) {
+  if ((data.round >= 16 && data.phase != 'over' && !teamSwaped) || (data.round <= 15 && teamSwaped)) {
     $('#teamT').attr('id', 'oldTeamT');
     $('#teamCT').attr('id', 'teamT');
     $('#oldTeamT').attr('id', 'teamCT');
@@ -40,17 +40,17 @@ socket.on('swapTeams', function(data){
 })
 
 socket.on('players', function(data) {
-  updatePlayers(data.ct,"CT");
-  updatePlayers(data.t,"T");
+  updatePlayers(data.ct, "CT");
+  updatePlayers(data.t, "T");
 });
 
-socket.on('playersImages', function(data){
+socket.on('playersImages', function(data) {
   for (var key in data.players) {
-    $(".player[steamid='"+data.players[key].steamid+"'] .playerImage").attr("src", data.players[key].image);
+    $(".player[steamid='" + data.players[key].steamid + "'] .playerImage").attr("src", data.players[key].image);
   }
 });
 
-socket.on('noData', function(){
+socket.on('noData', function() {
   $("#status").text("Waiting for data...");
 });
 
@@ -75,49 +75,51 @@ socket.on('bombStatus', function(data) {
 function notifyUser(text) {
   if ("Notification" in window) {
     if (Notification.permission === "granted") {
-      var notification = new Notification("CSGO Spectator", {body : text});
-    }
-    else {
-      Notification.requestPermission(function (permission) {
-      if (permission === "granted") {
-        var notification = new Notification("CSGO Spectator", {body : text});
-      }
-    });
+      var notification = new Notification("CSGO Spectator", {
+        body: text
+      });
+    } else {
+      Notification.requestPermission(function(permission) {
+        if (permission === "granted") {
+          var notification = new Notification("CSGO Spectator", {
+            body: text
+          });
+        }
+      });
     }
   }
 }
 
-function updatePlayers(players, team){
+function updatePlayers(players, team) {
   for (var i = 0; i < players.length; i++) {
     var player = players[i];
-    var id = "#team"+team+" .player"+i;
+    var id = "#team" + team + " .player" + i;
     $(id).attr("steamid", player.steamid);
-    $(id+" .playerInfo .playerName").text(player.name);
-    $(id+" .playerMoney").text(player.money);
+    $(id + " .playerInfo .playerName").text(player.name);
+    $(id + " .playerMoney").text(player.money);
     // Health
-    if(player.health == 0){
+    if (player.health == 0) {
       $(id).addClass("dead");
-    }
-    else {
+    } else {
       $(id).removeClass("dead");
     }
-    $(id+" .playerHealth .progress-bar").attr("aria-valuenow", player.health);
-    $(id+" .playerHealth .progress-bar").css("width", player.health + "%");
-    $(id+" .playerHealth .progress-bar").text(player.health);
+    $(id + " .playerHealth .progress-bar").attr("aria-valuenow", player.health);
+    $(id + " .playerHealth .progress-bar").css("width", player.health + "%");
+    $(id + " .playerHealth .progress-bar").text(player.health);
     // Armor
-    $(id+" .playerArmor .progress-bar").attr("aria-valuenow", player.armor);
-    $(id+" .playerArmor .progress-bar").css("width", player.armor + "%");
-    $(id+" .playerArmor .progress-bar").text(player.armor);
-    $(id+" .playerImage").css("filter", function(){
+    $(id + " .playerArmor .progress-bar").attr("aria-valuenow", player.armor);
+    $(id + " .playerArmor .progress-bar").css("width", player.armor + "%");
+    $(id + " .playerArmor .progress-bar").text(player.armor);
+    $(id + " .playerImage").css("filter", function() {
       var flashInt = parseInt(player.flashed);
-      var flashValue = 100+(flashInt*3);
-      return "brightness("+flashValue+"%)";
+      var flashValue = 100 + (flashInt * 3);
+      return "brightness(" + flashValue + "%)";
     });
     // More info
-    $(id+" .playerKills").text(player.kills);
-    $(id+" .playerDeaths").text(player.deaths);
-    $(id+" .playerAssists").text(player.assists);
-    $(id+" .playerScore").text(player.score);
+    $(id + " .playerKills").text(player.kills);
+    $(id + " .playerDeaths").text(player.deaths);
+    $(id + " .playerAssists").text(player.assists);
+    $(id + " .playerScore").text(player.score);
     // $(".player"+i+" .playerName").text(player.roundKills);
   }
 }
@@ -142,13 +144,12 @@ $('#hideLog').click(function() {
 });
 
 $('#showOptions').click(function() {
-  console.log($('#showOptions').attr('enabled') )
-  // We show the options if the button wasn't clicked
-  if($('#showOptions').attr('enabled') == 'false'){
+  console.log($('#showOptions').attr('enabled'))
+    // We show the options if the button wasn't clicked
+  if ($('#showOptions').attr('enabled') == 'false') {
     $('.option').show();
     $('#showOptions').attr('enabled', 'true');
-  }
-  else {
+  } else {
     $('.option').hide();
     $('#showOptions').attr('enabled', 'false');
   }
